6326c75d830e588c8d6624ba1b24d2372e5480da
Son Truong <sont85@gmail.com>
2015-10-11 21:03:58 -0700

    more updates to payment stripe, WIP

--- public/javascripts/payment.jsx
+++ public/javascripts/payment.jsx
@@ -3,11 +3,17 @@ var Payment = React.createClass({
   getInitialState: function() {
     return {
       charging: false,
-      errorMessage: null
+      errorMessage: null,
+      amount: 2900
     };
   },
   componentDidMount: function() {
     Stripe.setPublishableKey('pk_test_Ij3se1Hg07Oc3g4mlIwamvLT');
+  },
+  handleAmount: function(e) {
+    this.setState({
+      amount: e.target.value
+    });
   },
   handleSubmit: function(e) {
     e.preventDefault();
@@ -23,10 +29,23 @@ var Payment = React.createClass({
         errorMessage: response.error.message,
         charging: false
       });
+      swal('Payment Failed', response.error.message , 'error');
     } else {
-      $.post('/payment', {stripeToken: response.id, amount: 2900}, function(response) {
+      $.post('/payment', {
+        stripeToken: response.id,
+        amount: this.state.amount
+      }, (function(response) {
         console.log(response);
-      });
+        if (response === 'success') {
+          $('#payment').modal('hide');
+          swal('Payment Successful', 'Thank you, receipt will be email to you.', 'success');
+        } else {
+          swal('Payment Failed', response , 'error');
+          this.setState({
+            charging: false
+          });
+        }
+      }).bind(this));
     }
   },
   render: function() {
@@ -42,6 +61,10 @@ var Payment = React.createClass({
             <form action="/" id="payment-form" method="POST" onSubmit={this.handleSubmit}>
               <span className="payment-errors">{this.state.errorMessage}</span>
               <div className="form-row">
+                <div>
+                  <label><input name='amount' onChange={this.handleAmount} type='radio' value='2900' required/>$29</label>
+                  <label><input name='amount' onChange={this.handleAmount} type='radio' value='5900' required/>$59</label>
+                </div>
                 <label>
                   <span>Card Number</span>
                   <input data-stripe="number" size="20" type="text"/>
@@ -58,12 +81,10 @@ var Payment = React.createClass({
                   <span>Expiration (MM/YYYY)</span>
                   <input data-stripe="exp-month" size="2" type="text"/>
                 </label>
-                <span>
-                  /
-                </span>
+                <span>/</span>
                 <input data-stripe="exp-year" size="4" type="text"/>
               </div>
-              <button type="submit" disabled={this.state.charging}>Submit Payment</button>
+              <button disabled={this.state.charging} type="submit">Submit Payment</button>
             </form>
           </div>
         </div>
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
-
+var stripe = require('stripe')(process.env.TEST_SECRET_KEY);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.post('/payment', function(req, res) {
+  var charge = stripe.charges.create({
+    amount: req.body.amount,
+    currency: 'usd',
+    source: req.body.stripeToken,
+    receipt_email: 'sont85@gmail.com',
+    description: 'Example charge, JOHN SMITH 29.99 AUGUST'
+  }, function(err, charge) {
+    if (err && err.type === 'StripeCardError') {
+      console.log('------------',err);
+      res.json(err.message);
+    } else {
+      console.log(charge);
+      res.json('success');
+    }
+  });
 });
 
 module.exports = router;
